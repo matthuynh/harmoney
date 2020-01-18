691b005f1bfc516708ba5c1f23a636328b05c950
Matthew Huynh <mattrhuynh@gmail.com>
2020-01-18 13:35:35 -0500

    Push again

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -5,7 +5,7 @@ const router = express.Router();
  * @route         GET /create
  * @description   A user can create a room. The user must include their user id when sending the request.
  * From the req, we can get the user id. Create a room and associate it with that user id. Assign an id
- * to this new room. Return the room id.
+ * to this new room. Return the room idÍ.
  */
 router.get("/create", async (req, res) => {
 
