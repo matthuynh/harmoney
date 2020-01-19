bfb6703886a811c5c232ab32af938afad539f4f1
Matthew Huynh <mattrhuynh@gmail.com>
2020-01-19 02:49:24 -0500

    Add state and fetch (request/response code) to RegisterScreen.js

--- client/babel.config.js
+++ client/babel.config.js
@@ -4,3 +4,11 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
   };
 };
+
+// For using react-native-dotenv
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv'
+  ]
+}
